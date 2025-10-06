95c1a360caa0f394b3e3d5b8ae38d044ccac8e49
azuki774 <azuki774s@gmail.com>
2025-10-06 16:40:55 +0900

    fixup! use since-id

--- azkey-bot-roumu/azkey_bot_roumu/commands.py
+++ azkey-bot-roumu/azkey_bot_roumu/commands.py
@@ -103,7 +103,7 @@ def serve_command(interval):
                 logger.info(
                     f'action=check_execute cycle={cycle_count} read_latest_id={read_latest_id} message="Executing check operations"'
                 )
-                timeline, read_latest_id = usecases.get_timeline(
+                timeline, next_read_latest_id = usecases.get_timeline(
                     limit=100, since_id=read_latest_id
                 )
                 notes_count = len(timeline) if timeline else 0
@@ -151,6 +151,7 @@ def serve_command(interval):
                         f"already_count={already_checked_in} "
                         f"failure_count={failed_checkins}"
                     )
+                    read_latest_id = next_read_latest_id  # タイムラインを読み切ったので、次以降読まないように
                 else:
                     logger.info(
                         f'action=timeline_empty cycle={cycle_count} message="Timeline is empty"'
